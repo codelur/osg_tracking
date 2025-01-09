9b69694a77f6c82502772b925189f91615201430
Sal <codelur@gmail.com>
2025-01-09 13:20:46 -0800

    test module explanation

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
-
+//default test that rent relevant for this app
 describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
